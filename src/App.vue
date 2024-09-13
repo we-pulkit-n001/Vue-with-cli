a111c861d38d47b71ad76b21dad69dd3709ef016
Pulkit Nagpal <pulkit-n001@webreinvent.com>
2024-09-13 16:39:20 +0530

    Added props, emits, click events, slots

---
+++ src/App.vue
@@ -0,0 +1,59 @@
+<template>
+  <input type="text" ref="name">
+  <!-- <button @click="handleClick">click me</button> -->
+   <Teleport to=".modals" v-if="showModal"> 
+    <!-- <AppModal :header='header' :text='text' @close="toggleModal"/> -->
+    <AppModal @close="toggleModal">
+      <h1>This is heading 1</h1>
+      <p>This is paragraph</p>
+    </AppModal>
+  </Teleport>
+  <button @click="toggleModal">Show/hide Modal</button>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+
+import AppModal from './components/AppModal.vue';
+
+export default {
+  name: 'App',
+  data(){
+    return {
+      header: 'Signup',
+      text: 'Trial purpose',
+      showModal: false
+    }
+  },
+  components:{
+    AppModal
+  },
+  methods:{
+    handleClick(){
+        console.log(this.$refs.name)
+        this.$refs.name.classList.add("active")
+        this.$refs.name.focus()
+    },
+    toggleModal(){
+      this.showModal = !this.showModal
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1 {
+  display: inline-block;
+  padding-bottom: 10px;
+  border-bottom: 1px solid #ddd;
+}
+</style>
---
+++ src/components/AppModal.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="backdrop" @click.self="closeModal">
+        <div class="modal">
+            <p>Modal Content</p>
+            <h1>{{ header }}</h1>
+            <p>{{ text }}</p>
+        </div>
+    </div>
+    <slot></slot>
+</template>
+
+<script>
+    export default{
+        props:['header','text'],
+        methods: {
+            closeModal(){
+                this.$emit('close')
+            }
+        }
+    }
+</script>
+
+
+<style>
+.modal{
+    width: 400px;
+    padding: 20px;
+    margin: 100px auto;
+    background: white;
+    border-radius: 10px;
+}
+
+.backdrop{
+    top: 0;
+    position: fixed;
+    background: rgba(0,0,0,0.5);
+    width: 100%;
+    height: 100%;
+}
+</style>
